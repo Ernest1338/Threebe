6c2a1e935de289d963144de1ff05d4f0228ddf96
Ernest Gupik <ernestgupik@wp.pl>
2020-04-27 21:46:52 +0000

    missing comma fix

--- x86/x86opcodesTable.py
+++ x86/x86opcodesTable.py
@@ -5,5 +5,5 @@ x86opcodes = {
     "74" : "JE",
     "55" : "PUSH ebp",
     "B8" : "MOV eax", # next bite represents MOV value / if 4 next bites are not instructions, then those bites represent an adress in format: B8 AB CD EF GH > MOV eax, 0xHGFCDAB
-    "89" : "MOV ebp" # if bite is equal to 89 then check if next bite is equal to E5 / if the next bite is equal to E5 then "MOV ebp, esp"
+    "89" : "MOV ebp", # if bite is equal to 89 then check if next bite is equal to E5 / if the next bite is equal to E5 then "MOV ebp, esp"
 }
