a6a7f823d6fb69ee27f16288765c86620954fb11
Ernest Gupik <ernestgupik@wp.pl>
2020-04-28 15:06:26 +0000

    fixed spacing between offsets, bytes and instructions / added cancel_iteration function

--- x86/disassembly.py
+++ x86/disassembly.py
@@ -1,28 +1,39 @@
 import x86.x86opcodesTable as x86opT
 from hexdump.hexdump import bcolors
 
+times = 0
+
+def cancle_function_iteration(howmany):
+    global times
+    times += int(howmany)
+
 def disassemble(bytes):
+    global times
     offset1 = 2152202240
     counter1 = 0
     for i in bytes:
-        to_display = i
-        after_instruction = ""
-        after_byte = ""
-        if i in x86opT.x86opcodes:
-            if i == "74": # JE
-                after_byte = " "+bytes[counter1+1]
-                after_instruction = " "+hex(offset1+(int(bytes[counter1+1],16)+2))
-                check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
-                if len(check1) < 63:
-                    for _ in range(63-len(check1)):
-                        after_byte += " "
-                check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
-                print(check1)
-                after_byte = ""
-                after_instruction = ""
+        if times == 0:
+            to_display = i
+            after_instruction = ""
+            after_byte = ""
+            if i in x86opT.x86opcodes:
+                if i == "74": # JE
+                    after_byte = " "+bytes[counter1+1]
+                    after_instruction = " "+hex(offset1+(int(bytes[counter1+1],16)+2))
+                    check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
+                    if len(check1) < 63:
+                        for _ in range(63-len(check1)):
+                            after_byte += " "
+                    check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
+                    print(check1)
+                    after_byte = ""
+                    after_instruction = ""
+                    cancle_function_iteration(1)
+            else:
+                #pass
+                print(f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+"                "+f"{bcolors.WARNING}"+"???"+f"{bcolors.ENDC}")
         else:
-            pass
-            #print(f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+"               "+f"{bcolors.WARNING}"+"???"+f"{bcolors.ENDC}")
+            times -= 1
         offset1 += 16
         counter1 += 1
     # print(x86opT.x86opcodes["55"])
