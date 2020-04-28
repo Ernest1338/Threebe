42b8116cb3af1952e374f3461721b04ab55e6fe0
Ernest Gupik <ernestgupik@wp.pl>
2020-04-28 14:38:23 +0000

    added JE instruction / fixes

--- x86/disassembly.py
+++ x86/disassembly.py
@@ -1,5 +1,29 @@
 import x86.x86opcodesTable as x86opT
+from hexdump.hexdump import bcolors
 
 def disassemble(bytes):
-    print(x86opT.x86opcodes["55"])
+    offset1 = 2152202240
+    counter1 = 0
+    for i in bytes:
+        to_display = i
+        after_instruction = ""
+        after_byte = ""
+        if i in x86opT.x86opcodes:
+            if i == "74": # JE
+                after_byte = " "+bytes[counter1+1]
+                after_instruction = " "+hex(offset1+(int(bytes[counter1+1],16)+2))
+                check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
+                if len(check1) < 63:
+                    for _ in range(63-len(check1)):
+                        after_byte += " "
+                check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
+                print(check1)
+                after_byte = ""
+                after_instruction = ""
+        else:
+            pass
+            #print(f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+"               "+f"{bcolors.WARNING}"+"???"+f"{bcolors.ENDC}")
+        offset1 += 16
+        counter1 += 1
+    # print(x86opT.x86opcodes["55"])
     return True
--- x86/x86opcodesTable.py
+++ x86/x86opcodesTable.py
@@ -3,7 +3,15 @@
 x86opcodes = {
 
     "74" : "JE",
+    "75" : "JNE",
     "55" : "PUSH ebp",
-    "B8" : "MOV eax", # next bite represents MOV value / if 4 next bites are not instructions, then those bites represent an adress in format: B8 AB CD EF GH > MOV eax, 0xHGFCDAB
-    "89" : "MOV ebp", # if bite is equal to 89 then check if next bite is equal to E5 / if the next bite is equal to E5 then "MOV ebp, esp"
+    "B8" : "MOV eax", # next byte represents MOV value / if 4 next bytes are not instructions, then those bytes represent an adress in format: B8 AB CD EF GH > MOV eax, 0xHGFCDAB
+    "89" : "MOV ebp", # if byte is equal to 89 then check if next byte is equal to E5 / if the next byte is equal to E5 then "MOV ebp, esp"
+    "0F" : "INSTRUCTION",
+}
+
+x860Fopcodes = {
+
+    "84" : "JE",
+
 }
