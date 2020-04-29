4d645d5b8a58548fcb5325132977c6f559386009
Ernest Gupik <ernestgupik@wp.pl>
2020-04-29 19:29:39 +0000

    Added support for the PUSH ebp instruction / improvements on the visuals performance

--- x86/disassembly.py
+++ x86/disassembly.py
@@ -17,12 +17,14 @@ def disassemble(bytes):
             after_instruction = ""
             after_byte = ""
             if i in x86opT.x86opcodes:
+                intruction_len_for_check = 50+len(x86opT.x86opcodes[i]) # need to add to this after_instruction every time this variable (after_instruction) is usesd inside an if
                 if i == "74": # JE
                     after_byte = " "+bytes[counter1+1]
                     after_instruction = " "+hex(offset1+(int(bytes[counter1+1],16)+2))
                     check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
-                    if len(check1) < 63:
-                        for _ in range(63-len(check1)):
+                    intruction_len_for_check = 50+len(x86opT.x86opcodes[i])+len(after_instruction)
+                    if len(check1) < intruction_len_for_check:
+                        for _ in range(intruction_len_for_check-len(check1)):
                             after_byte += " "
                     check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
                     print(check1)
@@ -31,8 +33,16 @@ def disassemble(bytes):
                     cancle_function_iteration(1)
                 elif i == "90": # NOP
                     check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
-                    if len(check1) < 53:
-                        for _ in range(53-len(check1)):
+                    if len(check1) < intruction_len_for_check:
+                        for _ in range(intruction_len_for_check-len(check1)):
+                            after_byte += " "
+                    check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
+                    print(check1)
+                    after_byte = ""
+                elif i == "55": # PUSH ebp
+                    check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
+                    if len(check1) < intruction_len_for_check:
+                        for _ in range(intruction_len_for_check-len(check1)):
                             after_byte += " "
                     check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
                     print(check1)
