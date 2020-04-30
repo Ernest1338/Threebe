857c1ff87bfbf1ab3f9858b4ba26bef9705fe4a3
Ernest Gupik <ernestgupik@wp.pl>
2020-04-30 17:21:19 +0000

    code readability improvements / fixed offsets in disassembly

--- x86/disassembly.py
+++ x86/disassembly.py
@@ -11,13 +11,17 @@ def disassemble(bytes):
     global times
     offset1 = 2152202240
     counter1 = 0
+
     for i in bytes:
+
         if times == 0:
             to_display = i
             after_instruction = ""
             after_byte = ""
+
             if i in x86opT.x86opcodes:
                 intruction_len_for_check = 50+len(x86opT.x86opcodes[i]) # need to add to this after_instruction every time this variable (after_instruction) is usesd inside an if
+
                 if i == "74": # JE
                     after_byte = " "+bytes[counter1+1]
                     after_instruction = " "+hex(offset1+(int(bytes[counter1+1],16)+2))
@@ -31,6 +35,7 @@ def disassemble(bytes):
                     after_byte = ""
                     after_instruction = ""
                     cancle_function_iteration(1)
+
                 elif i == "90": # NOP
                     check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
                     if len(check1) < intruction_len_for_check:
@@ -39,6 +44,7 @@ def disassemble(bytes):
                     check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
                     print(check1)
                     after_byte = ""
+
                 elif i == "55": # PUSH ebp
                     check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
                     if len(check1) < intruction_len_for_check:
@@ -47,12 +53,15 @@ def disassemble(bytes):
                     check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
                     print(check1)
                     after_byte = ""
+                    
             else:
                 pass
                 #print(f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+"                "+f"{bcolors.WARNING}"+"???"+f"{bcolors.ENDC}")
+
         else:
             times -= 1
-        offset1 += 16
+        offset1 += 1
         counter1 += 1
+
     # print(x86opT.x86opcodes["55"])
     return True
