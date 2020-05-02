e0da73b07a473b8aace2390bceda9424dff07aa0
Ernest Gupik <ernestgupik@wp.pl>
2020-05-02 19:17:30 +0000

    Update on x86opcodesTable: added most of the MOV instrucions (with 8B opcode).

--- x86/x86opcodesTable.py
+++ x86/x86opcodesTable.py
@@ -2,51 +2,60 @@
 
 x86opcodes = {
 
-    "74" : "JE",         # DONE?
-    "75" : "JNE",        # PENDING
-    "50" : "PUSH eax",   # DONE
-    "51" : "PUSH ecx",   # DONE
-    "52" : "PUSH edx",   # DONE
-    "53" : "PUSH ebx",   # DONE
-    "54" : "PUSH esp",   # DONE
-    "55" : "PUSH ebp",   # DONE
-    "56" : "PUSH esi",   # DONE
-    "57" : "PUSH edi",   # DONE
-    "0E" : "PUSH cs",    # DONE
-    "16" : "PUSH ss",    # DONE
-    "1E" : "PUSH ds",    # DONE
-    "06" : "PUSH es",    # DONE
-    "1F" : "POP ds",     # DONE
-    "07" : "POP es",     # DONE
-    "17" : "POP ss",     # DONE
-    "58" : "POP eax",    # DONE
-    "59" : "POP ecx",    # DONE
-    "5A" : "POP edx",    # DONE
-    "5B" : "POP ebx",    # DONE
-    "5C" : "POP esp",    # DONE
-    "5D" : "POP ebp",    # DONE
-    "5E" : "POP esi",    # DONE
-    "5F" : "POP edi",    # DONE
-    "61" : "POPAL",      # DONE
-    "B8" : "MOV eax",    # PENDING  # next byte represents MOV value / if 4 next bytes are not instructions, then those bytes represent an adress in format: B8 AB CD EF GH > MOV eax, 0xHGFCDAB
-    "89" : "MOV ebp",    # PENDING  # if byte is equal to 89 then check if next byte is equal to E5 / if the next byte is equal to E5 then "MOV ebp, esp"
-    "01" : "ADD",        # DONE?    # if next byte is equal to CA then: ADD EDX, ECX
-                                    #             -||-      to D0 then: ADD EAX, EDX
-    "83" : "ADD",        # PENDING  # if nexy byte is equal to C2 then: ADD EDX, <value>  where VALUE is equal to byte after C2
-                                    #             -||-      to C4 then: ADD ESP, <VALUE>             -||-
-                                    #             -||-      to C7 then: ADD EDI, <VALUE>             -||-
-                                    # if next 2 bytes: 45 FC <value> then: ADD DWORD [var_4h], <value>
-    "90" : "NOP",        # DONE
-    "81" : "ADD",        # PENDING  # if next
+    "74" : "JE",            # DONE?
+    "75" : "JNE",           # PENDING
+    "50" : "PUSH eax",      # DONE
+    "51" : "PUSH ecx",      # DONE
+    "52" : "PUSH edx",      # DONE
+    "53" : "PUSH ebx",      # DONE
+    "54" : "PUSH esp",      # DONE
+    "55" : "PUSH ebp",      # DONE
+    "56" : "PUSH esi",      # DONE
+    "57" : "PUSH edi",      # DONE
+    "0E" : "PUSH cs",       # DONE
+    "16" : "PUSH ss",       # DONE
+    "1E" : "PUSH ds",       # DONE
+    "06" : "PUSH es",       # DONE
+    "1F" : "POP ds",        # DONE
+    "07" : "POP es",        # DONE
+    "17" : "POP ss",        # DONE
+    "58" : "POP eax",       # DONE
+    "59" : "POP ecx",       # DONE
+    "5A" : "POP edx",       # DONE
+    "5B" : "POP ebx",       # DONE
+    "5C" : "POP esp",       # DONE
+    "5D" : "POP ebp",       # DONE
+    "5E" : "POP esi",       # DONE
+    "5F" : "POP edi",       # DONE
+    "61" : "POPAL",         # DONE
+    "B8" : "MOV eax",       # PENDING  # next byte represents MOV value / if 4 next bytes are not instructions, then those bytes represent an adress in format: B8 AB CD EF GH > MOV eax, 0xHGFCDAB
+    "BA" : "MOV edx",       # PENDING  # next byte represents MOV value
+    "8B" : "MOV",           # PENDING  # if next byte is equal to 1C then: MOV ebx, dword [esp]
+                                       #             -||-      to 10 then: MOV edx, dword [eax]
+                                       #             -||-      to 55 then: MOV edx, dword [var_4h]
+                                       #             -||-      to 45 then: MOV eax, dword [arg_8h]
+                                       #             -||-      to 4D then: MOV ecx, dword [var_4h]
+                                       #             -||-      to 00 then: MOV eax, dword [eax]
+                                       #             -||-      to 6C then: MOV ebp, dword [arg_4h]
+                                       #             -||-      to 43 then: MOV eax, dword [ebx + <VALUE>] where <VALUE> is equal to byte after 43
+    "89" : "MOV ebp",       # PENDING  # if byte is equal to 89 then check if next byte is equal to E5 / if the next byte is equal to E5 then "MOV ebp, esp"
+    "01" : "ADD",           # DONE?    # if next byte is equal to CA then: ADD EDX, ECX
+                                       #             -||-      to D0 then: ADD EAX, EDX
+    "83" : "ADD",           # PENDING  # if nexy byte is equal to C2 then: ADD EDX, <value>  where VALUE is equal to byte after C2
+                                       #             -||-      to C4 then: ADD ESP, <VALUE>             -||-
+                                       #             -||-      to C7 then: ADD EDI, <VALUE>             -||-
+                                       # if next 2 bytes: 45 FC <value> then: ADD DWORD [var_4h], <value>
+    "90" : "NOP",           # DONE
+    "81" : "ADD",           # PENDING  # if next
 
-    "0F" : "INSTRUCTION",# PENDING
+    "0F" : "INSTRUCTION",   # PENDING
 }
 
 x860Fopcodes = {
 
-    "84" : "JE",         # PENDING
-    "A0" : "PUSH fs",    # PENDING
-    "A8" : "PUSH gs",    # PENDING
-    "A1" : "POP fs",     # PENDING
-    "A9" : "POP gs",     # PENDING
+    "84" : "JE",            # PENDING
+    "A0" : "PUSH fs",       # PENDING
+    "A8" : "PUSH gs",       # PENDING
+    "A1" : "POP fs",        # PENDING
+    "A9" : "POP gs",        # PENDING
 }
