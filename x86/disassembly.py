0246468092ef24bd96b5466670bd00051c8a9a23
Ernest Gupik <ernestgupik@wp.pl>
2020-05-05 19:52:04 +0000

    Added support for ADD instruction (with opcode 83) / fixed displaying hex numbers.

--- x86/disassembly.py
+++ x86/disassembly.py
@@ -72,7 +72,7 @@ def disassemble(bytes):
                     BAvar = bytes[counter1+1]
                     if bytes[counter1+1][0]=="0":
                         BAvar = bytes[counter1+1][1]
-                    after_instruction = ", "+str(BAvar)
+                    after_instruction = ", "+"0x"+str(BAvar)
                     check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
                     intruction_len_for_check = 50+len(x86opT.x86opcodes[i])+len(after_instruction)
                     if len(check1) < intruction_len_for_check:
@@ -83,6 +83,31 @@ def disassemble(bytes):
                     after_byte = ""
                     after_instruction = ""
                     cancle_function_iteration(1)
+
+                elif i == "83": # ADD
+                    after_byte = " "+bytes[counter1+1]+" "+bytes[counter1+2]
+                    ADD83var = bytes[counter1+2]
+                    if bytes[counter1+2][0]=="0":
+                        ADD83var = bytes[counter1+2][1]
+                    if bytes[counter1+1]=="C2":
+                        after_instruction = " edx, "+"0x"+str(ADD83var.lower())
+                    elif bytes[counter1+1]=="C4":
+                        after_instruction = " esp, "+"0x"+str(ADD83var.lower())
+                    elif bytes[counter1+1]=="C7":
+                        after_instruction = " edi, "+"0x"+str(ADD83var.lower())
+                    else:
+                        should_print = False
+                    check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
+                    intruction_len_for_check = 50+len(x86opT.x86opcodes[i])+len(after_instruction)
+                    if len(check1) < intruction_len_for_check:
+                        for _ in range(intruction_len_for_check-len(check1)):
+                            after_byte += " "
+                    check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
+                    if should_print:
+                        print(check1)
+                    after_byte = ""
+                    after_instruction = ""
+                    cancle_function_iteration(2)
                     
             else:
                 pass
