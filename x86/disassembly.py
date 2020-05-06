f6449330b944d8f1b601c7ad5672636b8cd75720
Ernest Gupik <ernestgupik@wp.pl>
2020-05-06 20:40:50 +0000

    Added support for JNE instruction (75 opcode) / Added missing instructions explanations comments.

--- x86/disassembly.py
+++ x86/disassembly.py
@@ -108,6 +108,20 @@ def disassemble(bytes):
                     after_byte = ""
                     after_instruction = ""
                     cancle_function_iteration(2)
+
+                elif i == "75": # JNE
+                    after_byte = " "+bytes[counter1+1]
+                    after_instruction = " "+hex(offset1+(int(bytes[counter1+1],16)+2))
+                    check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
+                    intruction_len_for_check = 50+len(x86opT.x86opcodes[i])+len(after_instruction)
+                    if len(check1) < intruction_len_for_check:
+                        for _ in range(intruction_len_for_check-len(check1)):
+                            after_byte += " "
+                    check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+x86opT.x86opcodes[i]+after_instruction+f"{bcolors.ENDC}"
+                    print(check1)
+                    after_byte = ""
+                    after_instruction = ""
+                    cancle_function_iteration(1)
                     
             else:
                 pass
--- x86/x86opcodesTable.py
+++ x86/x86opcodesTable.py
@@ -2,8 +2,8 @@
 
 x86opcodes = {
 
-    "74" : "JE",            # DONE?
-    "75" : "JNE",           # PENDING
+    "74" : "JE",            # DONE?    # next byte represents how much instructions to jump (+2). Example: 74 05 -> JE <ADDRESS OF THIS INSTRUCTION>+7 (05+2=7)
+    "75" : "JNE",           # PENDING  # next byte represents how much instructions to jump (+2). Example: 75 05 -> JNE <ADDRESS OF THIS INSTRUCTION>+7 (05+2=7)
     "50" : "PUSH eax",      # DONE
     "51" : "PUSH ecx",      # DONE
     "52" : "PUSH edx",      # DONE
