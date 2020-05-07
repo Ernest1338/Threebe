9d465f1d09219aee756818348c40c199ef018066
Ernest Gupik <ernestgupik@wp.pl>
2020-05-07 10:59:35 +0000

    Added bytes coloring in hex view.

--- hexdump/hexdump.py
+++ hexdump/hexdump.py
@@ -179,6 +179,7 @@ def hexdump_clean(hexdump):
 
 def hexdump_parser(hexdump):
     hexdump = str(hexdump)
+    print(hexdump)
     parsed1 = hexdump[2:-1]
     parsed2 = []
     flag1 = False
@@ -232,38 +233,71 @@ def hexdump_parser(hexdump):
                 parsed3 += str(hex(ord(i))[2:])+" "
         else:
             ascii1 = ""
-            parsed4 = ""
+            parsed4 = "" # colored version of the hex view (old)
+            parsed5 = []
             for a in parsed3.split(" "):
                 try:
                     if chr(int(a,16)).isprintable():
                         ascii1 += f"{bcolors.WARNING}"+chr(int(a,16))+f"{bcolors.ENDC}"
                         parsed4 += f"{bcolors.WARNING}"+str(a)+f"{bcolors.ENDC}"+" "
+                        parsed5.append(True)
                     else:
                         ascii1 += "."
                         parsed4 += str(a)+" "
+                        parsed5.append(False)
                 except:
                     pass
-            parsed3 = parsed3[:12]+""+parsed3[11:24]+" "+parsed3[24:36]+" "+parsed3[36:]
-            print(f"{bcolors.OKBLUE}"+str(hex(offset1))+f"{bcolors.ENDC}  "+parsed3.upper()+f"{bcolors.OKBLUE}| {bcolors.ENDC}"+ascii1) #len - 51
+            parsed6 = parsed3[:-1].split(" ")
+            for b in range(len(parsed6)):
+                if parsed5[b] == True:
+                    parsed6[b] = f"{bcolors.WARNING}"+str(parsed3.split(" ")[b].upper())+f"{bcolors.ENDC}"+" "
+                else:
+                    parsed6[b] = str(parsed3.split(" ")[b].upper())+" "
+            parsed6.insert(4, " ")
+            parsed6.insert(9, " ")
+            parsed6.insert(14, " ")
+            parsed7 = "".join(parsed6)
+            # parsed3 = parsed3[:12]+""+parsed3[11:24]+" "+parsed3[24:36]+" "+parsed3[36:]
+            print(f"{bcolors.OKBLUE}"+str(hex(offset1))+f"{bcolors.ENDC}  "+parsed7+f"{bcolors.OKBLUE}| {bcolors.ENDC}"+ascii1) #len - 51
+            # print(parsed4[:-1].split(" ")) # to remove
+            # print(parsed5)
             offset1 += 16
             if len(i)==2:
                 parsed3 = str(i)+" "
             else:
                 parsed3 = str(hex(ord(i))[2:])+" "
     ascii1 = ""
+    parsed4 = "" # colored version of the hex view (old)
+    parsed5 = []
     for a in parsed3.split(" "):
         try:
             if chr(int(a,16)).isprintable():
-                ascii1 += chr(int(a,16))
+                ascii1 += f"{bcolors.WARNING}"+chr(int(a,16))+f"{bcolors.ENDC}"
+                parsed4 += f"{bcolors.WARNING}"+str(a)+f"{bcolors.ENDC}"+" "
+                parsed5.append(True)
             else:
                 ascii1 += "."
+                parsed4 += str(a)+" "
+                parsed5.append(False)
         except:
             pass
     try:
-        parsed3 = parsed3[:12]+""+parsed3[11:24]+" "+parsed3[24:36]+" "+parsed3[36:]
+        parsed6 = parsed3[:-1].split(" ")
+        for b in range(len(parsed6)):
+            if parsed5[b] == True:
+                parsed6[b] = f"{bcolors.WARNING}"+str(parsed3.split(" ")[b].upper())+f"{bcolors.ENDC}"+" "
+            else:
+                parsed6[b] = str(parsed3.split(" ")[b].upper())+" "
+        parsed6.insert(4, " ")
+        parsed6.insert(9, " ")
+        parsed6.insert(14, " ")
+        parsed7 = "".join(parsed6)
+        # parsed3 = parsed3[:12]+""+parsed3[11:24]+" "+parsed3[24:36]+" "+parsed3[36:]
     except:
         pass
-    for i in range(51-len(parsed3)):
-        parsed3 += " "
-    print(f"{bcolors.OKBLUE}"+str(hex(offset1))+f"{bcolors.ENDC}  "+parsed3.upper()+f"{bcolors.OKBLUE}| {bcolors.ENDC}"+ascii1)
+    for i in range(51-(len(parsed3)+3)):
+        parsed7 += " "
+    print(f"{bcolors.OKBLUE}"+str(hex(offset1))+f"{bcolors.ENDC}  "+parsed7+f"{bcolors.OKBLUE}| {bcolors.ENDC}"+ascii1)
+    # print(parsed4[:-1].split(" ")) # to remove
+    # print(parsed5)
     return True
