da16193006d2a632d86b39de16cb54ea5f4c3c99
Ernest Gupik <ernestgupik@wp.pl>
2020-05-10 18:06:04 +0000

    Fixed comment about JNE instruction (75) in x86opcoesTable.py file.

--- x86/x86opcodesTable.py
+++ x86/x86opcodesTable.py
@@ -3,7 +3,7 @@
 x86opcodes = {
 
     "74" : "JE",            # DONE?    # next byte represents how much instructions to jump (+2). Example: 74 05 -> JE <ADDRESS OF THIS INSTRUCTION>+7 (05+2=7)
-    "75" : "JNE",           # PENDING  # next byte represents how much instructions to jump (+2). Example: 75 05 -> JNE <ADDRESS OF THIS INSTRUCTION>+7 (05+2=7)
+    "75" : "JNE",           # DONE?    # next byte represents how much instructions to jump (+2). Example: 75 05 -> JNE <ADDRESS OF THIS INSTRUCTION>+7 (05+2=7)
     "50" : "PUSH eax",      # DONE
     "51" : "PUSH ecx",      # DONE
     "52" : "PUSH edx",      # DONE
