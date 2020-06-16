5a78a27f90c1163119b727db57d8dfb1a79158be
Ernest Gupik <ernestgupik@wp.pl>
2020-06-16 18:29:46 +0000

    Added support for: ADD dword [ecx], eax instruction (x86).

--- x86/disassembly.py
+++ x86/disassembly.py
@@ -63,6 +63,8 @@ def disassemble_x86(bytes, ascii_dict):
                         after_instruction = " edx, ecx"
                     elif bytes[counter1+1]=="D0":
                         after_instruction = " eax, edx"
+                    elif bytes[counter1+1]=="01":
+                        after_instruction = " dword [ecx], eax"
                     else:
                         should_print = False
                     check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+instruction+after_instruction+f"{bcolors.ENDC}"
