97738ddf493504934d327c8e6b25e4592650960e
Ernest Gupik <ernestgupik@wp.pl>
2020-06-29 20:59:26 +0000

    Added support for: CALL eax instruction (x86).

--- x86/x86disassembly.py
+++ x86/x86disassembly.py
@@ -293,6 +293,24 @@ def disassemble_x86(bytes, ascii_dict):
                     after_byte = ""
                     after_instruction = ""
                     cancle_function_iteration(1)
+
+                elif i == "FF": # CALL
+                    after_byte = " "+bytes[counter1+1]
+                    if bytes[counter1+1]=="D0":
+                        after_instruction = " eax"
+                    else:
+                        should_print = False
+                    check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+instruction+after_instruction+f"{bcolors.ENDC}"
+                    intruction_len_for_check = 50+len(instruction)+len(after_instruction)
+                    if len(check1) < intruction_len_for_check:
+                        for _ in range(intruction_len_for_check-len(check1)):
+                            after_byte += " "
+                    check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+instruction+after_instruction+f"{bcolors.ENDC}"
+                    if should_print:
+                        print(check1)
+                    after_byte = ""
+                    after_instruction = ""
+                    cancle_function_iteration(1)
                     
             else:
                 pass
