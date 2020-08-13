c9ba86d684da97d91694462e30e587485301def8
Ernest Gupik <ernestgupik@wp.pl>
2020-08-13 10:26:40 +0000

    Added support for: TEST ebp, eax instruction (x86). [#24]

--- x86/x86opcodesTable.py
+++ x86/x86opcodesTable.py
@@ -75,7 +75,7 @@ x86opcodes = {
                             # PENDING  #           -||-        to C8 then: TEST eax, ecx
                             # PENDING  #           -||-        to C7 then: TEST edi, eax
                             # PENDING  #           -||-        to C6 then: TEST esi, eax
-                            # PENDING  #           -||-        to C5 then: TEST ebp, eax
+                            # DONE?    #           -||-        to C5 then: TEST ebp, eax
                             # DONE?    #           -||-        to C4 then: TEST esp, eax
                             # DONE?    #           -||-        to C3 then: TEST ebx, eax
                             # DONE?    #           -||-        to C2 then: TEST edx, eax
