4b0bd29d15f580f70d102ba394751bdb7b6f9ac8
Ernest Gupik <ernestgupik@wp.pl>
2020-08-22 17:04:12 +0000

    Added support for: TEST esp, ecx instruction (x86). [#24]

--- x86/x86disassembly.py
+++ x86/x86disassembly.py
@@ -330,6 +330,8 @@ def disassemble_x86(bytes, ascii_dict):
                         after_instruction = " edx, ecx"
                     elif bytes[counter1+1]=="CB":
                         after_instruction = " ebx, ecx"
+                    elif bytes[counter1+1]=="CC":
+                        after_instruction = " esp, ecx"
                     else:
                         should_print = False
                     check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+instruction+after_instruction+f"{bcolors.ENDC}"
