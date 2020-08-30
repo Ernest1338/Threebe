218422051b56a70ec0d9ed084396b56716a3576a
Ernest Gupik <ernestgupik@wp.pl>
2020-08-30 18:36:52 +0000

    Updated instruction status.

--- x86/x86opcodesTable.py
+++ x86/x86opcodesTable.py
@@ -61,7 +61,7 @@ x86opcodes = {
                             # PENDING  #           -||-        to D6 then: TEST esi, edx
                             # PENDING  #           -||-        to D5 then: TEST ebp, edx
                             # PENDING  #           -||-        to D4 then: TEST esp, edx
-                            # PENDING  #           -||-        to D3 then: TEST ebx, edx
+                            # DONE?    #           -||-        to D3 then: TEST ebx, edx
                             # DONE?    #           -||-        to D2 then: TEST edx, edx
                             # DONE?    #           -||-        to D1 then: TEST ecx, edx
                             # DONE?    #           -||-        to D0 then: TEST eax, edx
