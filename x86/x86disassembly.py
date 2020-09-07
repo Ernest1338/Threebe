13f95f91a845a647859489eebf39f4d2740918a7
Ernest Gupik <ernestgupik@wp.pl>
2020-09-07 19:33:09 +0000

    Added support for: TEST ecx, ebx instruction (x86). [#24]

--- x86/x86disassembly.py
+++ x86/x86disassembly.py
@@ -356,6 +356,8 @@ def disassemble_x86(bytes, ascii_dict):
                         after_instruction = " edi, edx"
                     elif bytes[counter1+1]=="D8":
                         after_instruction = " eax, ebx"
+                    elif bytes[counter1+1]=="D9":
+                        after_instruction = " ecx, ebx"
                     else:
                         should_print = False
                     check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+instruction+after_instruction+f"{bcolors.ENDC}"
--- x86/x86opcodesTable.py
+++ x86/x86opcodesTable.py
@@ -55,7 +55,7 @@ x86opcodes = {
                             # PENDING  #           -||-        to DC then: TEST esp, ebx
                             # PENDING  #           -||-        to DB then: TEST ebx, ebx
                             # PENDING  #           -||-        to DA then: TEST edx, ebx
-                            # PENDING  #           -||-        to D9 then: TEST ecx, ebx
+                            # DONE?    #           -||-        to D9 then: TEST ecx, ebx
                             # DONE?    #           -||-        to D8 then: TEST eax, ebx
                             # DONE?    #           -||-        to D7 then: TEST edi, edx
                             # DONE?    #           -||-        to D6 then: TEST esi, edx
