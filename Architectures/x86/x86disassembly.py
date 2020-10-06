6663cb3c9dff227c6121fc7eac1d2b126ad39103
Ernest Gupik <ernestgupik@wp.pl>
2020-10-06 19:16:32 +0000

    Added support for: TEST esi, ebp instruction (x86). [#24]

--- Architectures/x86/x86disassembly.py
+++ Architectures/x86/x86disassembly.py
@@ -401,6 +401,8 @@ def disassemble_x86(bytes, ascii_dict):
                         after_instruction = " esp, ebp"
                     elif bytes[counter1+1]=="ED":
                         after_instruction = " ebp, ebp"
+                    elif bytes[counter1+1]=="EE":
+                        after_instruction = " esi, ebp"
                     else:
                         should_print = False
                     check1 = f"{bcolors.OKBLUE}"+str(hex(offset1))+"   "+f"{bcolors.FAIL}"+to_display+after_byte+f"{bcolors.WARNING}"+instruction+after_instruction+f"{bcolors.ENDC}"
--- Architectures/x86/x86opcodesTable.py
+++ Architectures/x86/x86opcodesTable.py
@@ -34,7 +34,7 @@ x86opcodes = {
                             # PENDING  #           -||-        to F1 then: TEST ecx, esi
                             # PENDING  #           -||-        to F0 then: TEST eax, esi
                             # PENDING  #           -||-        to EF then: TEST edi, ebp
-                            # PENDING  #           -||-        to EE then: TEST esi, ebp
+                            # DONE?    #           -||-        to EE then: TEST esi, ebp
                             # DONE?    #           -||-        to ED then: TEST ebp, ebp
                             # DONE?    #           -||-        to EC then: TEST esp, ebp
                             # DONE?    #           -||-        to EB then: TEST ebx, ebp
