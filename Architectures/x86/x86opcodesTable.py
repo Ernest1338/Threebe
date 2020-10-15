8a825ba819c0471a214628deb97398b1a7fd936b
Ernest Gupik <ernestgupik@wp.pl>
2020-10-15 20:55:17 +0000

    Added support for: TEST edi, esi instruction (x86). [#24]

--- Architectures/x86/x86opcodesTable.py
+++ Architectures/x86/x86opcodesTable.py
@@ -25,7 +25,7 @@ x86opcodes = {
                             # PENDING  #           -||-        to FA then: TEST edx, edi
                             # PENDING  #           -||-        to F9 then: TEST ecx, edi
                             # PENDING  #           -||-        to F8 then: TEST eax, edi
-                            # PENDING  #           -||-        to F7 then: TEST edi, esi
+                            # DONE?    #           -||-        to F7 then: TEST edi, esi
                             # DONE?    #           -||-        to F6 then: TEST esi, esi
                             # DONE?    #           -||-        to F5 then: TEST ebp, esi
                             # DONE?    #           -||-        to F4 then: TEST esp, esi
