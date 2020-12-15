252d43c600378993364859db2d5e12a74e206732
Ernest Gupik <ernestgupik@wp.pl>
2020-12-15 16:11:04 +0000

    Added support for changing color schemes + added new color scheme. [#53]

--- Functions/Colors.py
+++ Functions/Colors.py
@@ -1,19 +1,54 @@
 # This file contains all entries to colors used in this project.
 
+# Here you can change the color scheme.
+# Choose a number between 1 and 2
 colorScheme = 1
 
-def returnColors(colorScheme):
-    if colorScheme == 1:  # NORMAL
-        class bcolors:
-            HEADER = '\033[95m'
-            OKBLUE = '\033[94m'
-            OKGREEN = '\033[92m'
-            WARNING = '\033[93m'
-            FAIL = '\033[91m'
-            BOLD = '\033[01m'
-            UNDERLINE = '\033[04m'
-            RESET = '\033[00m'
-    elif colorScheme == 2:  # CLEAN
+def returnColors(colorS):
+    global colorScheme
+    if colorS == 1:  # NORMAL
+        # NOTES FOR MAKING COLOR SCHEMES:
+        # format: \033[<NUMBER>m
+        # 30 - black
+        # 31 - red/pink
+        # 32 - green
+        # 33 - gold
+        # 34 - blue
+        # 35 - pink
+        # 36 - light blue
+        # 37 - white/gray
+        # 90 - gray
+        # 91 - red/pink
+        # 92 - light green
+        # 93 - yellow
+        # 94 - blue/purple
+        # 95 - pink
+        # 96 - light blue
+        # 97,98 - white
+        if colorScheme == 1:  # COLOR SCHEME NR. 1
+            class bcolors:
+                HEADER = '\033[95m'
+                OKBLUE = '\033[94m'
+                OKGREEN = '\033[92m'
+                WARNING = '\033[93m'
+                FAIL = '\033[91m'
+                BOLD = '\033[01m'
+                UNDERLINE = '\033[04m'
+                RESET = '\033[00m'
+        elif colorScheme == 2:  # COLOR SCHEME NR. 2
+            class bcolors:
+                HEADER = '\033[97m'
+                OKBLUE = '\033[34m'
+                OKGREEN = '\033[32m'
+                WARNING = '\033[33m'
+                FAIL = '\033[90m'
+                BOLD = '\033[01m'
+                UNDERLINE = '\033[04m'
+                RESET = '\033[00m'
+        else:
+            print("\033[91mERROR: WRONG COLOR SCHEME SELECTED!'\033[00m")
+            quit()
+    elif colorS == 2:  # CLEAN
         class bcolors:
             HEADER = '\033[00m'
             OKBLUE = '\033[00m'
