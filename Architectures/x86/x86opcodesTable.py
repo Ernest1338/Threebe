404e6db305c21c5b712a8ba6184cf53fb684df21
Ernest Gupik <ernestgupik@wp.pl>
2021-03-15 10:16:24 +0000

    Added support for: OR byte [edx], al instruction (x86). [#24]

--- Architectures/x86/x86opcodesTable.py
+++ Architectures/x86/x86opcodesTable.py
@@ -23,7 +23,7 @@ x86opcodes = {
     "0C" : "OR",            # DONE?    # next byte represents OR value: OR al, <VALUE>
     "08" : "OR",            # DONE?    # if next byte is equal to 00 then: OR byte [eax], al
                             # DONE?    #           -||-        to 01 then: OR byte [ecx], al
-                            # PENDING  #           -||-        to 02 then: OR byte [edx], al
+                            # DONE?    #           -||-        to 02 then: OR byte [edx], al
                             # PENDING  #           -||-        to 03 then: OR byte [ebx], al
                             # PENDING  #           -||-        to 06 then: OR byte [esi], al
                             # PENDING  #           -||-        to 07 then: OR byte [edi], al
