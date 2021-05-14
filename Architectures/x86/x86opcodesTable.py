17527cc1594d6d4c7c468d8111e92d54cbbcb563
Ernest Gupik <ernestgupik@wp.pl>
2021-05-14 10:06:03 +0000

    Added support for: IDIV edi instruction (x86). [#24]

--- Architectures/x86/x86opcodesTable.py
+++ Architectures/x86/x86opcodesTable.py
@@ -81,7 +81,7 @@ x86opcodes = {
                             # DONE?    #           -||-        to D2 then: NOT dl
                             # DONE?    #           -||-        to D1 then: NOT cl
                             # DONE?    #           -||-        to D0 then: NOT al
-    "F7" : "NOT",           # PENDING  # if next byte is equal to FF then: IDIV edi
+    "F7" : "NOT",           # DONE?    # if next byte is equal to FF then: IDIV edi
                             # PENDING  #           -||-        to FE then: IDIV esi
                             # PENDING  #           -||-        to FD then: IDIV ebp
                             # PENDING  #           -||-        to FC then: IDIV esp
