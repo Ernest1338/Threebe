725e004ad166d1771f016953f60cfff5d443a658
Ernest Gupik <ernestgupik@wp.pl>
2021-05-21 12:58:04 +0000

    Added support for: IDIV eax instruction (x86). [#24]

--- Architectures/x86/x86opcodesTable.py
+++ Architectures/x86/x86opcodesTable.py
@@ -88,7 +88,7 @@ x86opcodes = {
                             # DONE?    #           -||-        to FB then: IDIV ebx
                             # DONE?    #           -||-        to FA then: IDIV edx
                             # DONE?    #           -||-        to F9 then: IDIV ecx
-                            # PENDING  #           -||-        to F8 then: IDIV eax
+                            # DONE?    #           -||-        to F8 then: IDIV eax
                             # PENDING  #           -||-        to F7 then: DIV edi
                             # PENDING  #           -||-        to F6 then: DIV esi
                             # PENDING  #           -||-        to F5 then: DIV ebp
