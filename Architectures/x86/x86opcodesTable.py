aaf6842d8ae644df05a3fa6437f1003d6980eda7
Ernest Gupik <ernestgupik@wp.pl>
2021-05-29 08:25:01 +0000

    Added support for: DIV eax instruction (x86). [#24]

--- Architectures/x86/x86opcodesTable.py
+++ Architectures/x86/x86opcodesTable.py
@@ -96,7 +96,7 @@ x86opcodes = {
                             # DONE?    #           -||-        to F3 then: DIV ebx
                             # DONE?    #           -||-        to F2 then: DIV edx
                             # DONE?    #           -||-        to F1 then: DIV ecx
-                            # PENDING  #           -||-        to F0 then: DIV eax
+                            # DONE?    #           -||-        to F0 then: DIV eax
                             # PENDING  #           -||-        to EF then: IMUL edi
                             # PENDING  #           -||-        to EE then: IMUL esi
                             # PENDING  #           -||-        to ED then: IMUL ebp
