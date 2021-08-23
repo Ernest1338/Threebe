4eada383a71ae2a23295d301605b11d3c00306ee
Ernest Gupik <ernestgupik@wp.pl>
2021-08-23 13:13:34 +0200

    Added support for: MOV ecx, ecx instruction (x86). [#24]

--- Architectures/x86/x86disassembly.py
+++ Architectures/x86/x86disassembly.py
@@ -990,6 +990,8 @@ def disassemble_x86(bytes, ascii_dict, colors):
                         after_instruction = " edi, eax"
                     elif bytes[counter1+1]=="C8":
                         after_instruction = " eax, ecx"
+                    elif bytes[counter1+1]=="C9":
+                        after_instruction = " ecx, ecx"
                     elif bytes[counter1+1]=="CB":
                         after_instruction = " ebx, ecx"
                     elif bytes[counter1+1]=="E5":
--- Architectures/x86/x86opcodesTable.py
+++ Architectures/x86/x86opcodesTable.py
@@ -245,7 +245,7 @@ x86opcodes = {
                             # DONE?    #             -||-      to C6 then: MOV esi, eax
                             # DONE?    #             -||-      to C7 then: MOV edi, eax
                             # DONE?    #             -||-      to C8 then: MOV eax, ecx
-                            # PENDING  #             -||-      to C9 then: MOV ecx, ecx
+                            # DONE?    #             -||-      to C9 then: MOV ecx, ecx
                             # PENDING  #             -||-      to CA then: MOV edx, ecx
                             # DONE?    #             -||-      to CB then: MOV ebx, ecx
                             # PENDING  #             -||-      to CC then: MOV esp, ecx
